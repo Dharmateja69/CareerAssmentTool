f9d94219bb0096d6917383d7df7246be673361e3
Dharmateja69 <ddharmateja69@gmail.com>
2024-12-10 19:42:42 +0530

    Added and updated course-related features and views

--- src/main/java/com/klef/careerassessment/CareerassessmentApplication.java
+++ src/main/java/com/klef/careerassessment/CareerassessmentApplication.java
@@ -8,7 +8,7 @@ public class CareerassessmentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CareerassessmentApplication.class, args);
-		System.out.println("The spring is running on port 8081 !!");
+		System.out.println("The spring is running on port 8083 !!");
 	}
 
 
